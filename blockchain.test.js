9d12b7475f35237999f2f585108da57353e2febc
Romane G <romane71193@gmail.com>
2019-03-17 20:47:37 -0400

    tests for blockchain

--- blockchain.test.js
+++ blockchain.test.js
@@ -3,8 +3,10 @@ const Block = require('./block')
 
 describe('Blockchain', () => {
 
-  const blockchain = new Blockchain()
 
+  beforeEach(() => {
+   let blockchain = new Blockchain();
+  })
   it('contains a `chain array instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true);
   })
@@ -19,4 +21,43 @@ it('can add a block to chain', () => {
 
       expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData)
 })
+
+  describe('isValidChain()',() => {
+      describe('when the chain does not start with genesis block', () => {
+        it('returns false',() => {
+            blockchain.chain[0] = {data: 'fake-genesis'};
+            expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+        })
+      })
+    describe('chain starts with genesis block and has multiple blocks',() => {
+
+      beforeEach(() => {
+        blockchain.addBlock({ data: 'bears'})
+            blockchain.addBlock({ data: 'beets'})
+            blockchain.addBlock({ data: 'battlstar'})
+      })
+        describe('and lasthash refrence has changed', () => {
+          it('returns false',() => {
+           
+            expect(Blockchain.isValidchain(blockchain.chain)).toBe(false)
+          })
+        })
+          describe('contains block with invalid field', () => {
+            it('returns false',() => {
+            
+
+              blockchain.chain[2].data = 'some bad and evil data'
+            })
+            expect(Blockchain.isValidchain(blockchain.chain)).toBe(false)
+          })
+      
+    describe('and the chain does not contain any invalid blocks', () => {
+          it('returns true',() => {
+            
+
+              expect(Blockchain.isValidchain(blockchain.chain)).toBe(true)
+      })
+    })
+    })
+  })
 })
