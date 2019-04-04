c70c81b34ec4a9123eb53d44fac9c1a591724bfd
Romane G <romane71193@gmail.com>
2019-04-03 23:41:15 -0400

    update json

--- block.test.js
+++ block.test.js
@@ -3,7 +3,7 @@ const { GENESIS_DATA } = require('./config')
 const cryptoHash = require('./crypto-hash')
 
 describe('Block',() => {
-    const timestamp = 'a date';
+    const timestamp = 'a-date';
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain','data'];
