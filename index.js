301973819027351e646188671691bc2a638b74d1
Romane G <romane71193@gmail.com>
2019-06-11 23:43:53 -0400

    add rootchain on blockchain

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Blockchain = require('./blockchain')
+const request = require('request')
 const bodyParser = require('body-parser')
 const PubSub = require('./pubsub')
 
@@ -7,6 +8,8 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain })
 
+const DEFAULT_PORT = 3000
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 
 setTimeout(() => pubsub.broadcastChain(), 1000)
@@ -19,17 +22,29 @@ res.json(blockchain.chain);
 
 })
 
+const syncChains = () => {
+    request({url : `${ROOT_NODE_ADDRESS}/api/blocks` }, (error,response,body,) => {
+            if (!error && response.status === 200) {
+                const rootChain = JSON.parse(body)
+
+                console.log('replace chain on sync', rootChain);
+                blockchain.replaceChain(rootChain)
+            }
+    })
+}
+
 app.post('/api/mine',(req,res) => {
 
 const { data } = req.body;
 
 blockchain.addBlock({ data });
 
+pubsub.broadcastChain()
+
 res.redirect('/api/blocks')
 
 })
 
-const DEFAULT_PORT = 3000
 
 let PEER_PORT; 
 
@@ -39,6 +54,10 @@ if (process.env.GENERATE_PEER_PORT = 'true') {
 
 const PORT = PEER_PORT || DEFAULT_PORT;
 
-app.listen(PORT,() => (
-    console.log(`app is listening on localhost:${PORT}`))
-)
+app.listen(PORT,() => {
+
+    console.log(`app is listening on localhost:${PORT}`)
+
+    syncChains();
+
+})
