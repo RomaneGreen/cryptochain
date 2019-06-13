35abd2cad10c1f14ba424b20ed590902ef576163
Romane G <romane71193@gmail.com>
2019-06-13 15:58:36 -0400

    can now createTransaction

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require('./index')
 const  { verifySignature } = require('../util')
+const Transaction = require('./transaction')
 
 describe( 'wallet' ,() => {
 
@@ -45,4 +46,37 @@ describe('sigining data', () => {
     })
     
 })
+
+   describe('createTransaction()', () => {
+
+    describe(' and amount exceeds wallet balance',() => {
+            it('throws an error as a result', () => {
+                expect(() => wallet.createTransaction({amount: 999999, recipeient:'foo-recipient' })).toThrow('Amount exceeds balance') 
+
+                
+            })
+    })
+
+    describe(' and amount is valid',() => {
+
+        let transaction , amount, recipient;
+
+        beforeEach(() => {
+            amount = 50;
+            recipient = 'foo-bar';
+            transaction = wallet.createTransaction({ amount, recipient })
+        })
+        it(`create instance of transaction`, ()=> {
+                expect( transaction instanceof Transaction ).toBe(true)
+        })
+
+        it(`matches transaction input with wallet`, () => {
+                    expect(transaction.input.address).toEqual(wallet.publicKey)
+        })
+
+        it(`out puts the amount to the recipient`, () => {
+                expect(transaction.outputMap[recipient]).toEqual(amount)
+        })
+    })
+   })
 })
