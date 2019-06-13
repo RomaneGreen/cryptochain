029e909637f3ddca153cbe12e727fbcb240fff89
Romane G <romane71193@gmail.com>
2019-06-13 15:33:09 -0400

    so we dont use require statments to import functions?

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -2,20 +2,24 @@ const Transaction = require('./transaction')
 const Wallet = require('./index')
 const { verifySignature } = require('../util')
  
+
 describe('Transaction', () => {
+
     let transaction, senderWallet, recipient, amount;
 
-
     beforeEach(() => {
+
         senderWallet = new Wallet()
         recipient = 'recipient-public-key'
         amount = 50;
-
         transaction = new Transaction({ senderWallet, recipient, amount })
+
     })
 
     it('has an `id`', () => {
+
             expect(transaction).toHaveProperty('id')
+
     })
 
     
@@ -30,7 +34,6 @@ describe('Transaction', () => {
             expect(transaction.outputMap[recipient]).toEqual(amount)
         })
 
-
         it('outputs remaining balance for `sender wallet`',() => {
             expect(transaction.outputMap[senderWallet.publicKey]).toEqual(senderWallet.balance - amount)
         })
@@ -38,13 +41,14 @@ describe('Transaction', () => {
 
     describe('input', () => {
 
-
         it('has an input', () => {
                 expect(transaction).toHaveProperty('input')
         })
+
         it('has a timestamp', () => {
             expect(transaction.input).toHaveProperty('timestamp')
         })
+
         it('sets amount to sender wallet balance', () => {
             expect(transaction.input.amount).toEqual(senderWallet.balance)
         })
@@ -52,14 +56,50 @@ describe('Transaction', () => {
         it('sets adress to senderWallet publicKey', () => {
             expect(transaction.input.address).toEqual(senderWallet.publicKey)
         })
+
         it('signs the input', () => {
-            expect(
-            verifySignature({
-                publicKey :senderWallet.publicKey,
-                data: transaction.outputMap,
-                signature : transaction.input.signature
+            expect(verifySignature({ publicKey : senderWallet.publicKey, data: transaction.outputMap, signature : transaction.input.signature
             })).toBe(true)
-        })
+        })     
     })
 
+
+     describe('validTransaction()', () => {
+
+         let errorMock;
+
+        beforeEach(() => {
+            errorMock = jest.fn()
+            global.console.error = errorMock
+        })
+        describe('when transaction is valid', ()=> {
+                it('returns true', () => {
+                    expect(Transaction.validTransaction(transaction)).toBe(true)
+                })
+        })
+        describe('when transaction is Invalid', ()=> {
+
+            describe('and a outputMap is invalid',() => {
+
+                it('returns false and logs an error',() => {
+
+                    transaction.outputMap[senderWallet.publicKey] = 99999;
+                    expect(Transaction.validTransaction(transaction)).toBe(false)
+                    expect(errorMock).toHaveBeenCalled()
+                })
+
+            })
+        
+            describe('and the input signature is invalid', () => {
+
+                it('returns false and logs an error',() => {
+
+                    transaction.input.signature = new Wallet().sign('data')
+                    expect(Transaction.validTransaction(transaction)).toBe(false)
+                    expect(errorMock).toHaveBeenCalled()
+                })
+
+            })
+        })
+    })
 })
