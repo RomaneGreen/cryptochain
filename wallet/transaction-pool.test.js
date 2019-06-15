855a42455efdd694c0d6e4ace476949ad0b2eaef
Romane G <romane71193@gmail.com>
2019-06-14 22:30:47 -0400

    minor cngs

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,35 @@
+const TransactionPool = require('./transaction-pool');
+const Transaction = require('./transaction')
+const Wallet = require('./index')
+
+
+
+describe('transaction pool', () => {
+    let transaction, transactionPool, senderWallet;
+
+     beforeEach(() => {
+         senderWallet = new Wallet()
+         transactionPool = new TransactionPool()
+         transaction = new Transaction({
+             senderWallet: senderWallet,
+             recipient: 'fake-recipient',
+             amount: 50
+         })
+     })
+
+     describe('set transaction', () => {
+
+        it('adds a transaction', () => {
+            transactionPool.setTransaction(transaction)
+            expect(transactionPool.transactionMap[transaction.id]).toBe(transaction)
+        })
+     })
+
+     describe('existingTransaction()', () => {
+         it('returns an existing transaction given an input address', () => {
+             transactionPool.setTransaction(transaction)
+
+             expect(transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey })).toBe(transaction)
+         })
+     })
+})
