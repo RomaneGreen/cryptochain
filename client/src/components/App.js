6335451e950766ae358131a22fc582f5d941f908
Romane G <romane71193@gmail.com>
2019-06-18 11:58:38 -0400

    request data from api on frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom'
 
 
 export default class App extends Component {
+
+  state = {
+      walletInfo: { address: 'foov6', balance: 9999 }
+  }
+
+  
+  componentDidMount() {
+    fetch('http://localhost:3000/api/wallet-info').then(response => response.json()).then( json => this.setState({
+        walletInfo: json
+    })
+    ) 
+    
+  }
+  
   render() {
+
+    const { address, balance } = this.state.walletInfo
     return (
       <div>
-        Welcome to the blockhain...
+      <div>  Welcome to the blockhain... </div>
+        <div>Address: {address} </div>
+         <div>Balance: {balance} </div>
       </div>
     )
   }
